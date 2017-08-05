b4e6c8637c78e2a3eb515c75d341e8dfd031b874
Maicol <maicolbattistini@live.it>
2017-08-05 18:37:24 +0200

    Add files via upload
    *main.py
    *settings.py: sistemati bug, completata l'implementazione del pulsante cambia del primo parametro
    *timetable.py: alcuni aggiustamenti e predisposizioni per il funzionamento della schermata. Restano ancora bug irrisolti.

--- settings.py
+++ settings.py
@@ -1,28 +1,65 @@
-#Importazione di Tkinter
+#Importazione di Tkinter e librerie utili
 from tkinter import *
 import tkinter.messagebox
+import os.path #serve per verificare se un file è presente o no
+import numpy as np #utility per il salvataggio delle impostazioni in un file .npy
+def salvataggio0():
+    try:
+        ds["ORE_MAX_GIORNATA"]=sv.get()
+        np.save('settings.npy', ds) 
+        tkinter.messagebox.showinfo(title="Successo!", message="Parametro modificato con successo!")
+        wcv.destroy()
+        v0["text"]=ds["ORE_MAX_GIORNATA"]
+    except:
+        tkinter.messagebox.showerror(title="ERRORE!", message="Si è verificato un errore durante la modifica del parametro. Riprovare o contattare lo sviluppatore!")
+
+def cambiaValore0():
+    global wcv
+    wcv=Toplevel()
+    wcv.title("Cambia valore - Impostazioni - School Life Diary")
+    wcv.iconbitmap('sld_icon_beta.ico')
+    wcv.geometry("%dx%d+%d+%d" % (400, 200, 600, 250))
+    etichetta1=Label(wcv, text="Sceglere il valore da attribuire al parametro:")
+    var=DoubleVar()
+    global sv
+    sv=Scale(wcv, variable=var, from_=4, to=8, orient=HORIZONTAL)
+    bt1=Button(wcv,text="SALVA", command=salvataggio0)
+    etichetta1.pack(padx=10, pady=10)
+    sv.pack(padx=10, pady=10)
+    bt1.pack(padx=10, pady=10)
+    wcv.mainloop()
+    
+def inizializza():
+    global ds
+    if not(os.path.exists(r"settings.npy")):
+        ds={"ORE_MAX_GIORNATA":5}
+        np.save('settings.npy', ds) 
+    ds=np.load('settings.npy').item()
+    v0["text"]=ds["ORE_MAX_GIORNATA"]
 #Creazione finestra
 def creaFinestra():
     ws=Toplevel()
     ws.title("Impostazioni - School Life Diary")
     ws.iconbitmap('sld_icon_beta.ico')
-    ws.geometry("%dx%d+%d+%d" % (400, 400, 600, 250))
+    ws.geometry("%dx%d+%d+%d" % (900, 400, 600, 250))
     fs=Frame(ws)
     fs.pack()
     la1=Label(fs,text="Parametro")
     la2=Label(fs,text="Valore attuale")
     la3=Label(fs,text="Descrizione")
     la4=Label(fs,text="Azioni")
-    la1.grid(row=0, column=0)
-    la2.grid(row=0, column=1)
-    la3.grid(row=0, column=2)
-    la4.grid(row=0, column=3)
-    p1=Label(fs,text="ORE_MAX_GIORNATA")
-    v1=Label(fs,text="5")
-    d1=Label(fs,text="Imposta il numero di ore massime per giornate da visualizzare nell'orario")
-    a1=Button(fs,text="Cambia")
-    p1.grid(row=1, column=0)
-    v1.grid(row=1, column=1)
-    d1.grid(row=1, column=2)
-    a1.grid(row=1, column=3)
+    la1.grid(row=0, column=0, padx=10, pady=10)
+    la2.grid(row=0, column=1, padx=10, pady=10)
+    la3.grid(row=0, column=2, padx=10, pady=10)
+    la4.grid(row=0, column=3, padx=10, pady=10)
+    p0=Label(fs,text="ORE_MAX_GIORNATA")
+    global v0
+    v0=Label(fs,text="5")
+    d0=Label(fs,text="Imposta il numero di ore massime per giornate da visualizzare nell'orario")
+    a0=Button(fs,text="Cambia", command=cambiaValore0)
+    p0.grid(row=1, column=0, padx=10, pady=10)
+    v0.grid(row=1, column=1, padx=10, pady=10)
+    d0.grid(row=1, column=2, padx=10, pady=10)
+    a0.grid(row=1, column=3, padx=10, pady=10)
+    inizializza()
     ws.mainloop()
--- timetable.py
+++ timetable.py
@@ -1,29 +1,48 @@
 #Importazione di Tkinter
 from tkinter import *
 import tkinter.messagebox
+import numpy as np
+import os.path
+def inizializza():
+    global ds
+    global dt
+    ds=np.load('settings.npy').item()
+    if not(os.path.exists(r"timetable.npy")):
+        dt={}
+        for x in range(1,ds["ORE_MAX_GIORNATA"]+1):
+            for i in range (x,7):
+                dt[[x,i]]=""
+        np.save('timetable.npy', dt) 
+    dt=np.load('timetable.npy').item()
 #Creazione finestra
 def creaFinestra():
+    inizializza()
     wt=Toplevel()
     wt.title("Orario scolastico - School Life Diary")
     wt.iconbitmap('sld_icon_beta.ico')
-    wt.geometry("%dx%d+%d+%d" % (300, 300, 600, 250))
+    wt.geometry("%dx%d+%d+%d" % (400, 300, 600, 250))
     ft=Frame(wt)
     ft.pack()
-    l1=Label(text="Lunedì")
-    l2=Label(text="Martedì")
-    l3=Label(text="Mercoledì")
-    l4=Label(text="Giovedì")
-    l5=Label(text="Venerdì")
-    l6=Label(text="Sabato")
-    l1.grid(row=0, column=1)
-    l2.grid(row=0, column=2)
-    l3.grid(row=0, column=3)
-    l4.grid(row=0, column=4)
-    l5.grid(row=0, column=5)
-    l6.grid(row=0, column=6)
-    i=0
-    x=1
+    l1=Label(ft,text="Lunedì")
+    l2=Label(ft,text="Martedì")
+    l3=Label(ft,text="Mercoledì")
+    l4=Label(ft,text="Giovedì")
+    l5=Label(ft,text="Venerdì")
+    l6=Label(ft,text="Sabato")
+    l1.grid(row=0, column=1, pady=10, padx=5)
+    l2.grid(row=0, column=2, pady=10, padx=5)
+    l3.grid(row=0, column=3, pady=10, padx=5)
+    l4.grid(row=0, column=4, pady=10, padx=5)
+    l5.grid(row=0, column=5, pady=10, padx=5)
+    l6.grid(row=0, column=6, pady=10, padx=5)
+    i=1
+    x=ds["ORE_MAX_GIORNATA"]
     while (i<x):
-        h=Button(text="")
+        h=Label(ft, text=str(i)+"° ora")
+        h.grid(row=i, column=0, padx=5, pady=5)
         i+=1
+    for c in range(1,x+1):
+        for i in range(c,7):
+            bh=Button(ft, text=dt[[c,i]], command=Cambiaorario)
+        
     wt.mainloop()
