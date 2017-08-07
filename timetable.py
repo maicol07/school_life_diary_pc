f6e952ebb8cb9e12b5bb64ec2f2a028c4a979442
Maicol <maicolbattistini@live.it>
2017-08-07 19:06:31 +0200

    Add files via upload
    BUG FIX e aggiunte

--- timetable.py
+++ timetable.py
@@ -3,15 +3,44 @@ from tkinter import *
 import tkinter.messagebox
 import numpy as np
 import os.path
+
+def Salvataggio(p,var):
+    try:
+        dt[p]=var.get()
+        np.save('timetable.npy', dt)
+        tkinter.messagebox.showinfo(title="Successo!", message="Salvataggio effettuato con successo!")
+        wtc.destroy()
+        creaFinestra()
+    except:
+        tkinter.messagebox.showerror(title="Errore!", message="Si è verificato un errore, riprovare oppure contattare lo sviluppatore")
+
+def CambiaOrario(p): #p è la posizione in coordinate x e y (tupla) del pulsante cliccato 
+    global wtc
+    wtc=Toplevel()
+    wtc.title("Modifica Orario - Orario scolastico - School Life Diary")
+    wtc.iconbitmap('sld_icon_beta.ico')
+    wtc.geometry("%dx%d+%d+%d" % (450, 200, 600, 250))
+    dg={1:"Lunedì",2:"Martedì",3:"Mercoledì",4:"Giovedì",5:"Venerdì",6:"Sabato"}
+    print(p)
+    l=Label(wtc, text="Inserire la materia da visualizzare nell'orario la "+str(p[1])+"a ora del "+dg[p[0]-1]+".")
+    l.pack(padx=10,pady=10)
+    var=StringVar(value="")
+    e=Entry(wtc, textvariable=var)
+    e.pack(padx=10,pady=10)
+    b=Button(wtc, text="SALVA", command=lambda: Salvataggio(p,var))
+    b.pack(padx=10,pady=10)
+    wtc.mainloop()
+
 def inizializza():
     global ds
     global dt
     ds=np.load('settings.npy').item()
     if not(os.path.exists(r"timetable.npy")):
         dt={}
-        for x in range(1,ds["ORE_MAX_GIORNATA"]+1):
-            for i in range (x,7):
-                dt[[x,i]]=""
+        dt["ORE_MAX_GIORNATA"]=ds["ORE_MAX_GIORNATA"]
+        for x in range(1,ds["ORE_MAX_GIORNATA"]+2):
+            for i in range (1,7):
+                dt[(x,i)]=""
         np.save('timetable.npy', dt) 
     dt=np.load('timetable.npy').item()
 #Creazione finestra
@@ -20,7 +49,7 @@ def creaFinestra():
     wt=Toplevel()
     wt.title("Orario scolastico - School Life Diary")
     wt.iconbitmap('sld_icon_beta.ico')
-    wt.geometry("%dx%d+%d+%d" % (400, 300, 600, 250))
+    wt.geometry("%dx%d+%d+%d" % (600, 300, 600, 250))
     ft=Frame(wt)
     ft.pack()
     l1=Label(ft,text="Lunedì")
@@ -37,12 +66,13 @@ def creaFinestra():
     l6.grid(row=0, column=6, pady=10, padx=5)
     i=1
     x=ds["ORE_MAX_GIORNATA"]
-    while (i<x):
+    for i in range(1,x+1):
         h=Label(ft, text=str(i)+"° ora")
         h.grid(row=i, column=0, padx=5, pady=5)
         i+=1
-    for c in range(1,x+1):
-        for i in range(c,7):
-            bh=Button(ft, text=dt[[c,i]], command=Cambiaorario)
-        
+    for c in range(1,x+2):
+        for i in range(1, x+1):
+            print((c,i))
+            bh=Button(ft, text=dt[(c,i)], width=10, command=lambda: CambiaOrario((c,i)))
+            bh.grid(row=i, column=c)
     wt.mainloop()
