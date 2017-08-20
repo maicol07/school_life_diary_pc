56ed360ee205362e6ae0003da9e1b38d8447d81a
Maicol <maicolbattistini@live.it>
2017-08-20 21:24:21 +0200

    Update subjects.py

--- subjects.py
+++ subjects.py
@@ -7,7 +7,7 @@ def delete():
     m=0
 
 def add():
-    m=0
+    m=0 #Per non far causare un errore nell'avvio del software, ho creato una variabile con valore 0
 
 def edit():
     m=0
@@ -31,9 +31,12 @@ def creaFinestra():
     lb.pack(padx=10,pady=10)
     fim2=Frame(wim)
     fim2.pack()
-    bAdd=Button(fim2,image=PhotoImage(r"icons/add_FAB.png"),command=add)
-    bMod=Button(fim2,image=PhotoImage(r"icons/edit_FAB.png"),command=edit)
-    bDel=Button(fim2,image=PhotoImage(r"icons/trash_FAB.png"),command=delete)
+    imageAdd=PhotoImage(r"icons/add_FAB.png")
+    imageMod=PhotoImage(r"icons/edit_FAB.png")
+    imageDel=PhotoImage(r"icons/trash_FAB.png")
+    bAdd=Button(fim2,image=imageAdd,command=add)
+    bMod=Button(fim2,image=imageMod,command=edit)
+    bDel=Button(fim2,image=imageDel,command=delete)
     bAdd.grid(row=0,column=0,padx=10,pady=10)
     bMod.grid(row=0,column=1,padx=10,pady=10)
     bDel.grid(row=0,column=2,padx=10,pady=10)
