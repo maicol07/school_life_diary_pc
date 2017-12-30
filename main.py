bf477b80d188b759f8ceb5bdbbe67940184ca0c5
Maicol <maicolbattistini@live.it>
2017-12-30 23:05:05 +0100

    Aggiornato | Update
    main.py:
    ------------ IT
    - Implementazione SQLite3
    - Rimosso codice non necessario
    - Commentate varie sezioni
    ------------ EN
    - Implemented SQLite3
    - Removed not needed code
    - Commentate varie sezioni

--- main.py
+++ main.py
@@ -7,30 +7,42 @@
 #                   #
 #####################
 
-#Inizializzazione impostazioni e creazione file
-import gettext
-import os
-import ctypes
-import locale
-import PIL.Image, PIL.ImageTk
-import os.path
-import numpy as np
+#Inizializzazione impostazioni e creazione database
+
+import gettext, os, ctypes, locale, PIL.Image, PIL.ImageTk, webbrowser, time
+import sqlite3 as sql
 global pathset
-import tkinter.messagebox
-# filename for the file you want to save
-output_filename = "settings.npy"
+# Importazione tkinter e funzioni utili
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import Tk, Toplevel # per usare lo sfondo bianco
+import tkinter.messagebox as tkmb
+
+# CREAZIONE FILE (PRIMO AVVIO)
+
+output_filename = "settings.db"
 
 path = os.path.expanduser(r'~\Documents\School Life Diary')
-# check the directory does not exist
 if not(os.path.exists(path)):
-    # create the directory you want to save to
     os.mkdir(path)
 if not(os.path.exists(os.path.join(path,output_filename))):
-    ds = {"ORE_MAX_GIORNATA": 5}
-    # write the file in the new directory
-    np.save(os.path.join(path, output_filename), ds)
+    if os.path.exists(os.path.join(path,"settings.npy")):
+        tkmb.showwarning(title=_("Attenzione! Database non presente!"),
+                                       message=_("Non hai effettuato la migrazione del database. Il programma si avvierà, ma non saranno visualizzati i tuoi dati fino a che non effettuerai la migrazione."))
+        rd=tkmb.askokcancel(title=_("Conferma download strumento migrazione database"),
+                                       message=_("Vuoi scaricare lo strumento di migrazione del database?"))
+        if rd==True:
+            webbrowser.open("https://github.com/maicol07/school_life_diary_pc/releases")
+    fs=open(os.path.join(path, output_filename), "w")
+    fs.close()
+    conn=sql.connect(os.path.join(path, output_filename))
+    c=conn.cursor()
+    c.execute("""CREATE TABLE `settings` ( `setting` TEXT,
+                                                   `value` TEXT );""")
+    c.execute("INSERT INTO settings (setting,value) VALUES ('ORE_MAX_GIORNATA','5'); ")
 
-#Selezione lingua
+
+# INSTALLAZIONE LINGUA
 
 if not(os.path.exists(os.path.join(path,"language.txt"))):
     windll = ctypes.windll.kernel32
@@ -42,20 +54,15 @@ else:
         lgcode=fl.readline()
         lg = gettext.translation('main', localedir=os.path.join(path,'locale'), languages=[lgcode])
     except Exception as ex:
-        tkinter.messagebox.showerror(title=_("Lingua non impostata!"),
+        tkmb.showerror(title=_("Lingua non impostata!"),
                                      message=_(r"La lingua impostata non è stata riconosciuta. Per risolvere prova a eliminare il file in Documenti/School Life Diary/language.txt . Se il problema non si risolve, contattare lo sviluppatore. Errore: ")+str(ex))
 lg.install()
 
-# Importazione tkinter e funzioni utili
-import webbrowser
-from tkinter import *
-from tkinter.ttk import Style, Progressbar
-import settings
-import subjects
-import timetable
-import note
+# IMPORTAZIONE FILE ESTERNI
+import settings, subjects, timetable, note
 
-#Funzioni
+
+# FINESTRA INFORMAZIONI
 def info():
     wi=Toplevel()
     wi.configure(bg="white")
@@ -88,57 +95,45 @@ def info():
     l4.pack(padx=10,pady=15)
     bt.pack(padx=10)
 
-#Creazione della finestra, del frame e dei widget
+
+# CREAZIONE FINESTRA MENU PRINCIPALE
 global w
-#Verifica se esistono degli aggiornamenti per il programma
+# Verifica se esistono degli aggiornamenti per il programma
 v="0.3.0.1"
 import feedparser
 from subprocess import check_output
 feed_name="School Life Diary Releases"
 url=r"https://github.com/maicol07/school_life_diary_pc/releases.atom"
 feed=feedparser.parse(url)
-wp=Tk()
-wp.iconbitmap("images\sld_icon_beta.ico")
+try:
+    post=feed.entries[0]
+    title=post.title
+    lp=title.split(" ")
+    if (lp[0][1:].isdecimal()==True):
+        lp[0]==lp[0][1:]
+    if (v!=lp[0][1:]):
+        agg=tkmb.askyesno(title=_("Nuova versione disponibile!"),
+                                       message=_("È disponibile una nuova versione di School Life Diary.")+_("""
+Ti consigliamo di aggiornare il prima possibile per non perdere le novità, i miglioramenti e le correzioni di problemi.
+Vuoi accedere alla pagina da cui scaricare l'aggiornamento alla versione""")+" "+lp[0][1:]+"?")
+        if (agg==True):
+            webbrowser.open("https://github.com/maicol07/school_life_diary_pc/releases/")
+except IndexError:
+    tkmb.showwarning(title=_("Nessuna connessione ad internet"),
+                                   message=_("Non è disponibile nessuna connessione ad internet per la ricerca degli aggiornamenti. La ricerca verrà ritentata la prossima volta che sarà riaperto il programma."))
+w=Tk()
+w.configure(bg="white")
+w.title("School Life Diary")
+w.iconbitmap(r"images/sld_icon_beta.ico")
+#w.geometry("335x325+200+100")
 s=Style()
 try:
     s.theme_use("vista")
 except:
     s.theme_use()
-pbv=0
-le=Label(wp,text=_("Ricerca aggiornamenti..."))
-le.pack(padx=10,pady=10)
-pb=Progressbar(wp, mode="determinate",variable=pbv)
-pb.pack(padx=10,pady=10)
-def checknewversion(pb,wp):
-    try:
-        post=feed.entries[0]
-        pb.step(20)
-        title=post.title
-        pb.step(20)
-        lp=title.split(" ")
-        pb.step(20)
-        if (lp[0][1:].isdecimal()==True):
-            lp[0]==lp[0][1:]
-        pb.step(20)
-        if (v!=lp[0][1:]):
-            agg=tkinter.messagebox.askyesno(title=_("Nuova versione disponibile!"),
-                                           message=_("È disponibile una nuova versione di School Life Diary.")+_("""
-    Ti consigliamo di aggiornare il prima possibile per non perdere le novità, i miglioramenti e le correzioni di problemi.
-    Vuoi accedere alla pagina da cui scaricare l'aggiornamento alla versione""")+" "+lp[0][1:]+"?")
-            if (agg==True):
-                webbrowser.open("https://github.com/maicol07/school_life_diary_pc/releases/")
-        pb.step(20)
-    except IndexError:
-        tkinter.messagebox.showwarning(title=_("Nessuna connessione ad internet"),
-                                       message=_("Non è disponibile nessuna connessione ad internet per la ricerca degli aggiornamenti. La ricerca verrà ritentata la prossima volta che sarà riaperto il programma."))
-    wp.destroy()
-wp.after(1,checknewversion(pb,wp))
-wp.mainloop()
-w=Tk()
-w.configure(bg="white")
-w.title("School Life Diary")
-w.iconbitmap(r"images/sld_icon_beta.ico")
-w.geometry("335x325+200+100")
+s.configure("TFrame",background="white")
+s.configure("TButton",height=100)
+s.configure("TLabel",background="white")
 mb=Menu(w)
 w.config(menu=mb)
 fm=Menu(mb,tearoff=0)
@@ -169,13 +164,11 @@ hm.add_command(label=_("Guida"), image=pguida,
 hm.add_command(label=_("Informazioni"), image=pinfo,
                compound="left",command=info)
 f=Frame(w)
-f.configure(bg="white")
 logo=PhotoImage(file=r"images/school_life_diary_splash.png")
 title=Label(f,image=logo)
 f.pack()
 title.pack(padx=10,pady=10)
 f2=Frame(w)
-f2.configure(bg="white")
 f2.pack()
 itime = PIL.Image.open(r"icons\timetable.png")
 ptime = PIL.ImageTk.PhotoImage(itime)
@@ -187,31 +180,29 @@ inote = PIL.Image.open(r"icons\notes.png")
 pnote = PIL.ImageTk.PhotoImage(inote)
 iagenda = PIL.Image.open(r"icons\agenda.png")
 pagenda = PIL.ImageTk.PhotoImage(iagenda)
-b0=Button(f2,text=_("ORARIO"), background="#FF6C6C", width=100, height=25, image=ptime,
+b0=Button(f2,text=_("ORARIO"), #background="#FF6C6C",
+          style="TButton",
+          image=ptime,
           compound="left",
           command=timetable.creaFinestra)
-b1=Button(f2,text=_("MATERIE"), background="#FFBD45", width=100, height=25, image=pmaterie,
+b1=Button(f2,text=_("MATERIE"), #background="#FFBD45",
+          image=pmaterie,
           compound="left",
           command=subjects.creaFinestra)
-b2=Button(f2,text=_("VOTI"), background="#ADD8E6", width=100, height=25, image=pvoti,
+b2=Button(f2,text=_("VOTI"), #background="#ADD8E6",
+          image=pvoti,
           compound="left", command=lambda: webbrowser.open('https://apps.maicol07.tk/app/sld/voti/'))
-b3=Button(f2,text=_("ANNOTAZIONI"), background="#C389C3", width=100, height=25, image=pnote,
+b3=Button(f2,text=_("ANNOTAZIONI"), #background="#C389C3",
+          image=pnote,
           compound="left", command=note.creaFinestra)
-b4=Button(f2,text=_("AGENDA"), background="#7DFB7D", width=100, height=25, image=pagenda,
+b4=Button(f2,text=_("AGENDA"), #background="#7DFB7D",
+          image=pagenda,
           compound="left", command=lambda: webbrowser.open('https://calendar.google.com'))
-bs=Button(f2,text=_("IMPOSTAZIONI"), background="light grey", width=100, height=25, image=psettings,
-          compound="left",
-          command=settings.creaFinestra)
-be=Button(f2,text=_("ESCI"), background="grey", width=100,image=pexit, height=25,
-          compound="left",
-          command=w.destroy)
 b0.grid(row=0, column=0)
 b1.grid(row=0, column=1)
 b2.grid(row=0, column=2)
-b3.grid(row=1, column=0)
-b4.grid(row=1, column=1)
-bs.grid(row=1, column=2)
-be.grid(row=2, column=1)
+b3.grid(row=0, column=3)
+b4.grid(row=0, column=4)
 
 #Avvia il programma
 w.mainloop()
