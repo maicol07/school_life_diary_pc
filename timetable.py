ef07d016a5dc4d619df3bf02a5d37f4f6a66f9b6
Maicol <maicolbattistini@live.it>
2018-03-29 21:16:47 +0200

    Updated timetable.py
    - Improved UI
    - Compatibility with SQLite, removed NumPy support
    - Other fixes and enhancements.

--- timetable.py
+++ timetable.py
@@ -1,5 +1,5 @@
 import sqlite3 as sql
-import os.path, gettext, ctypes, locale, PIL.Image, PIL.ImageTk
+import os.path, gettext, ctypes, locale, PIL.Image, PIL.ImageTk, tk_tools
 
 global path
 global fn_set
@@ -42,22 +42,14 @@ else:
 lg.install()
 
 
-def Salvataggio(p, mat, mode="save"):
+def Salvataggio(matl, matm, matmm, matg, matv, mats):
     try:
-        conn = sql.connect(os.path.join(path, fn_set), isolation_level=None)
-        c = sconn.cursor()
-        matconn = sql.connect(os.path.join(path, "subjects.db"), isolation_level=None)
-        matc = matconn.cursor()
-        matc.execute("SELECT * FROM subjects")
-        lmat = matc.fetchall()
-        '''for i in range(len(lmat)):
-            if lmat[i[1]] == mat:
-                break
-            else:
-                continue'''
+        conn = sql.connect(os.path.join(path, fn_time), isolation_level=None)
+        c = conn.cursor()
         gg = ["Lun", "Mar", "Mer", "Gio", "Ven", "Sab"]
-        c.execute("""UPDATE subjects SET {} = "{}"
-WHERE ID={}; """.format(gg[p[1]], mat, p[0]))
+        mat = [matl, matm, matmm, matg, matv, mats]
+        for i in range(len(mat)):
+            c.execute("""UPDATE timetable SET {} = '{}' WHERE ID={}; """.format(gg[i], mat[i], sel["text"][0]))
         tkmb.showinfo(title=_("Successo!"),
                       message=_("Salvataggio effettuato con successo!"))
         wtc.destroy()
@@ -65,9 +57,7 @@ WHERE ID={}; """.format(gg[p[1]], mat, p[0]))
         creaFinestra()
     except Exception as ex:
         tkmb.showerror(title=_("Errore!"),
-                       message=_(
-                           "Si è verificato un errore, riprovare oppure contattare lo sviluppatore.\nErrore: {}").format(
-                           ex))
+                       message=_("Si è verificato un errore, riprovare oppure contattare lo sviluppatore.\nErrore: {}").format(ex))
 
 
 def updtcblist(e, m):
@@ -78,15 +68,20 @@ def updtcblist(e, m):
     e["values"] = l
 
 
-def CambiaOrario(p):  # p è la posizione in coordinate y e x (tupla) del pulsante cliccato
-    global wtc
+def cambia_orario():
+    global sel
+    sel = tt.item(tt.focus())
+    if sel["text"] == "":
+        tkmb.showwarning(title=_("Nessuna riga selezionata!"),
+                         message=_("Non è stata selezionata nessuna riga. Si prega di selezionarne una per apportare modifiche."))
+        return ""
+    global wtc, matconn, matc
     wtc = Toplevel()
     wtc.configure(bg="white")
     wtc.title(_("Modifica Orario - Orario scolastico") + " - School Life Diary")
     wtc.iconbitmap(r"images/sld_icon_beta.ico")
-    wtc.geometry("450x200+600+250")
-    l = Label(wtc,
-              text=_("Inserire la materia da visualizzare nell'orario la {}° ora del {}.".format(str(p[1]), dg[p[0]])))
+    wtc.geometry("700x200+600+250")
+    l = Label(wtc, text=_("Inserire le materie da visualizzare."))
     l.pack(padx=10, pady=10)
     try:
         matconn = sql.connect(os.path.join(path, "subjects.db"), isolation_level=None)
@@ -98,24 +93,47 @@ def CambiaOrario(p):  # p è la posizione in coordinate y e x (tupla) del pulsan
                        message=_(
                            "Errore! Nessuna materia inserita. Inserire delle materie dalla sezione materie!\nErrore specifico: {}").format(
                            str(ex)))
-    e = Combobox(wtc, set=dt[p[0]][p[1]], postcommand=lambda: updtcblist(e, mat))
-    e.pack(padx=10, pady=10)
+    dg = {1: _("Lunedì"), 2: _("Martedì"), 3: _("Mercoledì"), 4: _("Giovedì"), 5: _("Venerdì"), 6: _("Sabato")}
+    f=LabelFrame(wtc, text=_("Maschera di modifica"))
+    f.pack()
+    for i in dg:
+        lg = Label(f, text=dg[i])
+        lg.grid(row=0, column=i)
+    lo = Label(f, text=sel["text"])
+    lo.grid(row=1, column=0)
+
+    # Creazione di varie combobox (una per ogni giorno), 6 in totale
+
+    el = Combobox(f, postcommand=lambda: updtcblist(el, mat), width=10)
+    el.set(sel["values"][0])
+    el.grid(row=1, column=1, padx=10, pady=10)
+    em = Combobox(f, postcommand=lambda: updtcblist(em, mat), width=10)
+    em.set(sel["values"][1])
+    em.grid(row=1, column=2, padx=10, pady=10)
+    emm = Combobox(f, postcommand=lambda: updtcblist(emm, mat), width=10)
+    emm.set(sel["values"][2])
+    emm.grid(row=1, column=3, padx=10, pady=10)
+    eg = Combobox(f, postcommand=lambda: updtcblist(eg, mat), width=10)
+    eg.set(sel["values"][3])
+    eg.grid(row=1, column=4, padx=10, pady=10)
+    ev = Combobox(f, postcommand=lambda: updtcblist(ev, mat), width=10)
+    ev.set(sel["values"][4])
+    ev.grid(row=1, column=5, padx=10, pady=10)
+    es = Combobox(f, postcommand=lambda: updtcblist(es, mat), width=10)
+    es.set(sel["values"][5])
+    es.grid(row=1, column=6, padx=10, pady=10)
     f = LabelFrame(wtc, text=_("Azioni"))
     f.pack(padx=10, pady=10)
     psave = PIL.Image.open(r"icons/save.png")
     isave = PIL.ImageTk.PhotoImage(psave)
-    pdel = PIL.Image.open(r"icons/delete.png")
-    idel = PIL.ImageTk.PhotoImage(pdel)
-    b = Button(f, text=_("SALVA"), image=isave, compound=LEFT, command=lambda: Salvataggio(p, e.get()))
+    b = Button(f, text=_("SALVA"), image=isave, compound=LEFT, command=lambda: Salvataggio(el.get(), em.get(), emm.get(), eg.get(), ev.get(), es.get()))
     b.grid(row=0, column=0, padx=10, pady=10)
-    bd = Button(f, text=_("ELIMINA"), image=idel, compound=LEFT, command=lambda: Salvataggio(p, e, mode="del"))
-    bd.grid(row=0, column=1, padx=10, pady=10)
     matc.close()
     matconn.close()
     wtc.mainloop()
 
 
-def inizializza(conn, c):
+def inizializza(c):
     global ds
     ds = {}
     sconn = sql.connect(os.path.join(path, fn_set), isolation_level=None)
@@ -127,8 +145,7 @@ def inizializza(conn, c):
     c.execute("SELECT * FROM timetable")
     r = c.fetchall()
     dt = {}
-    '''Struttura dizionario:
-    {0:[MatLun,MatMar,MatMer,...],1:[...]}'''
+    # Struttura dizionario: {0:[MatLun,MatMar,MatMer,...],1:[...]}
     if not (r == []):
         for i in r:
             l = []
@@ -139,7 +156,7 @@ def inizializza(conn, c):
                     l.append(k)
             dt[i[0]] = l
     else:
-        for i in range(int(ds["ORE_MAX_GIORNATA"][0])):
+        for i in range(int(ds["ORE_MAX_GIORNATA"][0]) + 1):
             c.execute("""INSERT INTO timetable (Lun, Mar, Mer, Gio, Ven, Sab)
             VALUES ("", "", "", "", "", ""); """)
             for a in range(int(ds["ORE_MAX_GIORNATA"][0])):
@@ -152,42 +169,96 @@ def inizializza(conn, c):
     return dt
 
 
+# MENU TASTO DESTRO
+def popup(event):
+    if event.widget != tt:
+        return
+    # display the popup menu
+    try:
+        aMenu.tk_popup(event.x_root + 53, event.y_root, 0)
+    finally:
+        # make sure to release the grab (Tk 8.0a1 only)
+        aMenu.grab_release()
+
+
 def creaFinestra():
     """Creazione finestra principale"""
     conn = sql.connect(os.path.join(path, fn_time), isolation_level=None)
     cur = conn.cursor()
     global dt
-    dt = inizializza(conn, cur)
+    dt = inizializza(cur)
     global wt
     wt = Toplevel()
     wt.configure(bg="white")
     wt.title(_("Orario scolastico") + " - School Life Diary")
     wt.iconbitmap(r"images/sld_icon_beta.ico")
-    wt.geometry("600x300+600+250")
+    wt.geometry("700x300+600+250")
     s = Style()
     s.theme_use(ds["PC_THEME"][0])
     s.configure("TFrame", background="white")
     s.configure("TLabel", background="white")
-    s.configure("TPhotoimage", background="white")
+    s.configure("tthotoimage", background="white")
     s.configure("TLabelframe", background="white")
     s.configure("TLabelframe.Label", background="white")
     ft = Frame(wt)
     ft.pack()
-    global dg
-    dg = {1: _("Lunedì"), 2: _("Martedì"), 3: _("Mercoledì"), 4: _("Giovedì"), 5: _("Venerdì"), 6: _("Sabato")}
-    for i in range(1, len(dg) + 1):
-        l = Label(ft, text=dg[i])
-        l.grid(row=0, column=i, pady=10, padx=5)
-    i = 1
+    try:
+        matconn = sql.connect(os.path.join(path, "subjects.db"), isolation_level=None)
+        matc = matconn.cursor()
+        matc.execute("SELECT * FROM subjects")
+        mat = matc.fetchall()
+        listmat = []
+        for i in mat:
+            listmat.append(i[1])
+        listmat.sort()
+    except Exception as ex:
+        tkmb.showerror(title=_("Nessuna materia inserita!"),
+                       message=_(
+                           "Errore! Nessuna materia inserita. Inserire delle materie dalla sezione materie!\nErrore specifico: {}").format(
+                           str(ex)))
+        wt.destroy()
+        return
     x = ds["ORE_MAX_GIORNATA"][0]
+
+    # Creazione dei floating menu
+
+    iEdit = PhotoImage(file=r"icons/edit.png")
+    global aMenu
+    aMenu = Menu(wt, tearoff=0)
+    aMenu.add_command(label=_('Modifica riga'), image=iEdit, compound="left",
+                      command=cambia_orario)
+
+    global tt
+    tt = Treeview(wt)
+    tt.pack(padx=10, pady=10)
+    tt["columns"] = ("lun", "mar", "mer", "gio", "ven", "sab")
+    tt.column("#0", width=75)
+    tt.heading("lun", text=_("Lunedì"))
+    tt.column("lun", anchor=CENTER, width=90)
+    tt.heading("mar", text=_("Martedì"))
+    tt.column("mar", anchor=CENTER, width=90)
+    tt.heading("mer", text=_("Mercoledì"))
+    tt.column("mer", anchor=CENTER, width=90)
+    tt.heading("gio", text=_("Giovedì"))
+    tt.column("gio", anchor=CENTER, width=90)
+    tt.heading("ven", text=("Venerdì"))
+    tt.column("ven", anchor=CENTER, width=90)
+    tt.heading("sab", text=_("Sabato"))
+    tt.column("sab", anchor=CENTER, width=90)
+    tt.pack(padx=10, pady=10)
+    tt.bind("<Double-Button-1>", lambda e: cambia_orario())
+    tt.bind("<Button-3>", popup)
     for i in range(1, int(x) + 1):
-        h = Label(ft, text=str(i) + _("° ora"))
-        h.grid(row=i, column=0, padx=5, pady=5)
-        i += 1
-    for r in range(1, int(x) + 1):
-        for c in range(1, 7):
-            bh = Button(ft, text=dt[r - 1][c - 1], width=10, command=lambda c=c, r=r: CambiaOrario((c, r)))
-            bh.grid(row=r, column=c)
+        tt.insert("", i, text=_("{}° ora").format(i), values=[dt[i][0],
+                                                           dt[i][1],
+                                                           dt[i][2],
+                                                           dt[i][3],
+                                                           dt[i][4],
+                                                           dt[i][5]])
+    li = Label(wt, text=_(
+        "Per modificare l'orario, fai doppio click o usa il tasto destro del mouse su una riga e inserisci le materie dell'ora corrispondente."))
+    li.pack()
     cur.close()
     conn.close()
+    wt.focus()
     wt.mainloop()
